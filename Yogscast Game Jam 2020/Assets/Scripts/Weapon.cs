98b7ea8f2dc16233690e43a0a9f1d2af8f7c1003
isroScientist <67408817+isroScientist@users.noreply.github.com>
2020-12-05 15:07:44 +0530

    Added the weapon's rotation

---
+++ Yogscast Game Jam 2020/Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
