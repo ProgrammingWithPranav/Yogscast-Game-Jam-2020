ef24a6c306215d1c62fb08a5f79fae14fa49e9ba
isroScientist <67408817+isroScientist@users.noreply.github.com>
2020-12-05 23:34:00 +0530

    Added 1 secret path

---
+++ Yogscast Game Jam 2020/Assets/SecterPath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecterPath : MonoBehaviour
+{
+    public string sceneName;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Player")
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
