088f89860f9e70b2611d96d04be77ad92760ed78
isroScientist <67408817+isroScientist@users.noreply.github.com>
2020-12-05 19:43:15 +0530

    Added new Enemy

---
+++ Yogscast Game Jam 2020/Assets/Scripts/EnemyWeaponRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyWeaponRotation : MonoBehaviour
+{
+    public float rotationPerMinute;
+    public int damage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 6 * rotationPerMinute * Time.deltaTime);
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Player")
+		{
+            collision.GetComponent<PlayerController>().TakeDamage(damage);
+		}
+	}
+}
---
+++ Yogscast Game Jam 2020/Assets/Scripts/SpinningEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SpinningEnemy : MonoBehaviour
+{
+    public float speed;
+
+    Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Weapon")
+		{
+            Destroy(gameObject);
+		}
+	}
+}
