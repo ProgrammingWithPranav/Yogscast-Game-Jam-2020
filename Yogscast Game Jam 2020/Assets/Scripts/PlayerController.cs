530facd9031b398fd943c83c956ee80c8645c574
isroScientist <67408817+isroScientist@users.noreply.github.com>
2020-12-05 12:34:16 +0530

    Added Player Movement

---
+++ Yogscast Game Jam 2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    Vector2 moveAmount;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+    }
+
+	private void FixedUpdate()
+	{
+        rb.MovePosition(rb.position + moveAmount * Time.deltaTime);
+	}
+}
