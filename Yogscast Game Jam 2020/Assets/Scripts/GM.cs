2649a3e72af8d5db28c056db8161acae61d43285
isroScientist <67408817+isroScientist@users.noreply.github.com>
2020-12-06 22:08:45 +0530

    Added more UI

---
+++ Yogscast Game Jam 2020/Assets/Scripts/GM.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour
+{
+    public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Yogscast Game Jam 2020/Assets/Scripts/Link.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour
+{
+
+	public void OpenYoutubeChannel()
+	{
+#if !UNITY_EDITOR
+		openWindow("https://www.youtube.com/channel/UCJwogTIbU1K2BOLZUVfzp8w");
+#endif
+	}
+
+	public void OpenWebsite()
+	{
+#if !UNITY_EDITOR
+		openWindow("http://pranavtantri.tech/");
+#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
